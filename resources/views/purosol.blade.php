fcbc08731f9aa05fee3f9972d9b551a9a5eb8253
Ueslei <udias1984@gmail.com>
2020-09-23 23:55:26 -0400

    Ajustes na Pagina Inicial

--- resources/views/purosol.blade.php
+++ resources/views/purosol.blade.php
@@ -1,199 +1,206 @@
 <!doctype html>
-<html lang="pt">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
-
-    <title>A Pura Energia do Sol</title>
-
-    <!-- Principal CSS do Bootstrap -->
-    <link href="bootstrap.css" rel="stylesheet">
-
-    <link href="carrossel.css" rel="stylesheet">
-
-    <!-- Estilos customizados para esse template -->
-    <link href="carousel.css" rel="stylesheet">
-  </head>
-  <body>
-
-    <header>
-      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand" href="#">Carousel</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(atual)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#">Desativado</a>
-            </li>
-          </ul>
-          <form class="form-inline mt-2 mt-md-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
-          </form>
-        </div>
-      </nav>
-    </header>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+<link rel="icon" href="favicon.ico">
 
 
+<title>A Pura Energia do Sol</title>
 
-      <div id="Carousel" class="carousel slide" data-ride="carousel">
-
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-
-            <img src="imgs/sl_1.png" class= "img-fluid d-block">
-
-          </div>
-          <div class="carousel-item">
-
-            <img src="imgs/sl_2.png" class= "img-fluid d-block">
-
-          </div>
-          <div class="carousel-item">
-
-            <img src="imgs/sl_3.png" class= "img-fluid d-block">
-
-          </div>
-
-              <div class="carousel-caption text-left">
-                <h1>PuroSol Energia Solar, quem somos!</h1>
-                <p>A empresa PuroSol partiu da iniciativa de dois alunos do curso de ADS, Analise de Sesenvolvimento de Sistemas, da materia de desenvolvimento WEB, com a intensaão de trazer uma empresa que faça o diferencial das outras no ramo de Energia Solar.</p>
-                <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img class="second-slide" src="imgs/sl_2.png" alt="Second slide">
-            <div class="container">
-              <div class="carousel-caption">
-                <h1>Qualidade e Conforto!</h1>
-                <p>Buscamos o que há de melhor no mercado para lhe oferecer etrazer o maior conforto e segurança para viabilidade de seu projeto Solar.</p>
-                <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img class="third-slide" src="imgs/sl_3.png" alt="Third slide">
-            <div class="container">
-              <div class="carousel-caption text-right">
-                <h1>Acompanhamento de perto!!</h1>
-                <p>Desde o inicio até o final do seu projeto e instalação do sistema estaremos acompanhando para que você no cliente não tenha nenhuma comodidade, e que apenas usufrua do seu investimento. </p>
-                <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Voltar</span>
-        </a>
-        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Avançar</span>
-        </a>
-      </div>
+<!-- Principal CSS do Bootstrap -->
+<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
 
 
-      <!-- Mensagens de marketing e outras featurezinhas
-      ================================================== -->
-      <!-- Envolve o resto da página em outro container, para centralizar todo o conteúdo. -->
+<!-- Estilos customizados para esse template -->
+<link href="{{ asset('css/carousel.css') }}" rel="stylesheet">
 
-      <div class="container marketing">
+</head>
+<body>
 
-        <!-- Três colunas de texto, abaixo do carousel -->
-        <div class="row">
-          <div class="col-lg-4">
-            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-            <h2>Material de Qualidade</h2>
-            <p>Para fornecer a qualidade que você cliente precisa, escolhemos os melhores fornecedores com a mais alta qualidade e garantia do mercado Fotoltaico.</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-          </div><!-- /.col-lg-4 -->
-          <div class="col-lg-4">
-            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-            <h2>Profissionai Capacitados</h2>
-            <p>Todos nossos profissionais posuem estão prepardos par tirar todas as duvidas referente ao ramo solar, com capacitação direto dos nossos fornecedores, trazendo o conhecimento até você cliente, de forma clara e esclarecedora.</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-          </div><!-- /.col-lg-4 -->
-          <div class="col-lg-4">
-            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-            <h2>Instalação de Qualidade</h2>
-            <p>Buscamos não só o melho atendimento, o melhor material, mas também a melhor mão de obra para que seu investimento esteja sendo aplicado de forma segura e com toda responsabilidade, para entregar seu Sistema funcionando de forma perfeita.</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-          </div><!-- /.col-lg-4 -->
-        </div><!-- /.row -->
+<header>
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+<a class="navbar-brand" href="#">Contato</a>
+
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarCollapse">
+<ul class="navbar-nav mr-auto">
+<li class="nav-item active">
+<a class="nav-link" href="#">Home <span class="sr-only">(atual)</span></a>
+
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Link</a>
+
+</li>
+<li class="nav-item">
+<a class="nav-link disabled" href="#">Desativado</a>
+
+</li>
+</ul>
+<form class="form-inline mt-2 mt-md-0">
+<input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+</form>
+</div>
+</nav>
+</header>
+
+<main role="main">
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+<ol class="carousel-indicators">
+<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+<li data-target="#myCarousel" data-slide-to="1"></li>
+<li data-target="#myCarousel" data-slide-to="2"></li>
+</ol>
+<div class="carousel-inner">
+<div class="carousel-item active">
+<img class="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
+
+<div class="container">
+<div class="carousel-caption text-left">
+<h1>PuroSol Energia Solar. Quem Somos?</h1>
+<p>A empresa PuroSol partiu da iniciativa de dois alunos, Ueslei de Oliveira Dias e Ibsen Brilhante Freitas do curso de ADS, Analise de Desenvolvimento de Sistemas, da materia de desenvolvimento WEB, com a intensão de trazer uma empresa que faça o diferencial das outras no ramo de Energia Solar.</p>
+<p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+
+</div>
+</div>
+</div>
+<div class="carousel-item">
+<img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
+
+<div class="container">
+<div class="carousel-caption">
+<h1>Material de Qualidade</h1>
+<p>Para fornecer a qualidade que você cliente precisa, escolhemos os melhores fornecedores com a mais alta qualidade e garantia do mercado Fotovoltaico.</p>
+<p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+
+</div>
+</div>
+</div>
+<div class="carousel-item">
+<img class="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
+
+<div class="container">
+<div class="carousel-caption text-right">
+<h1>Profissionais</h1>
+<p>Todos nossos profissionais posuem estão prepardos par tirar todas as duvidas referente ao ramo solar, com capacitação direto dos nossos fornecedores, trazendo o conhecimento até você cliente, de forma clara e esclarecedora.</p>
+<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+
+</div>
+</div>
+</div>
+</div>
+<a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+<span class="sr-only">Voltar</span>
+</a>
+<a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+<span class="carousel-control-next-icon" aria-hidden="true"></span>
+<span class="sr-only">Avançar</span>
+</a>
+</div>
 
 
-        <!-- COMEÇAM AS MENCIONADAS FEATUREZINHAS xD -->
+<!-- Mensagens de marketing e outras featurezinhas
+================================================== -->
+<!-- Envolve o resto da página em outro container, para centralizar todo o conteúdo. -->
 
-        <hr class="featurette-divider">
+<div class="container marketing">
 
-       <!-- <div class="row featurette">
-          <div class="col-md-7">
-            <h2 class="featurette-heading">Primeiro título de featurezinhas. <span class="text-muted">Supreendente, não?!</span></h2>
-            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-          </div>
-          <div class="col-md-5">
-            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-          </div>
-        </div>
-
-        <hr class="featurette-divider">
-
-        <div class="row featurette">
-          <div class="col-md-7 order-md-2">
-            <h2 class="featurette-heading">Aêêê, moleque! <span class="text-muted">Tá legal ou não tá?</span></h2>
-            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-          </div>
-          <div class="col-md-5 order-md-1">
-            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-          </div>
-        </div> -->
-
-        <hr class="featurette-divider">
-
-        <div class="row featurette">
-          <div class="col-md-7">
-            <h2 class="featurette-heading">Entre em Contato, estaremos prontos para lhe atender. <span class="text-muted">Aguardamos você!</span></h2>
-            <p class="lead">A empresa PuroSol temo prazer em tirar todoas suas dúvidas e prestar um serviço de qualidade e responsabilidade.</p>
-          </div>
-          <div class="col-md-5">
-            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-          </div>
-        </div>
-
-        <hr class="featurette-divider">
-
-        <!-- /FIM DAS FEATUREZINHAS - -->
-
-      </div><!-- /.container -->
+<!-- Três colunas de texto, abaixo do carousel -->
+<div class="row">
+<div class="col-lg-4">
+<img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+<h2>Quem Somos?</h2>
+<p>A empresa PuroSol partiu da iniciativa de dois alunos, Ueslei de Oliveira Dias e Ibsen Brilhante Freitas do curso de ADS, Analise de Desenvolvimento de Sistemas, da materia de desenvolvimento WEB, com a intensão de trazer uma empresa que faça o diferencial das outras no ramo de Energia Solar.</p>
+<p><a class="btn btn-secondary" href="#" role="button">Ver mais &raquo;</a></p>
+</div><!-- /.col-lg-4 -->
+<div class="col-lg-4">
+<img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+<h2>Material de Qualidade</h2>
+<p>Para fornecer a qualidade que você cliente precisa, escolhemos os melhores fornecedores com a mais alta qualidade e garantia do mercado Fotovoltaico.</p>
+<p><a class="btn btn-secondary" href="#" role="button">Ver mais &raquo;</a></p>
+</div><!-- /.col-lg-4 -->
+<div class="col-lg-4">
+<img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+<h2>Profissionais</h2>
+<p>Todos nossos profissionais posuem estão prepardos par tirar todas as duvidas referente ao ramo solar, com capacitação direto dos nossos fornecedores, trazendo o conhecimento até você cliente, de forma clara e esclarecedora.</p>
+<p><a class="btn btn-secondary" href="#" role="button">Ver mais &raquo;</a></p>
+</div><!-- /.col-lg-4 -->
+</div><!-- /.row -->
 
 
-      <!-- FOOTER -->
-      <footer class="container">
-        <p class="float-right"><a href="#">Voltar ao topo</a></p>
-        <p>&copy; PuroSol, 2020 &middot; <a href="#">Privacidade</a> &middot; <a href="#">Termos</a></p>
-      </footer>
-    </main>
+<!-- COMEÇAM AS MENCIONADAS FEATUREZINHAS xD -->
 
-    <!-- Principal JavaScript do Bootstrap
-    ================================================== -->
-    <!-- Foi colocado no final para a página carregar mais rápido -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <script src="../../assets/js/vendor/popper.min.js"></script>
-    <script src="../../dist/js/bootstrap.min.js"></script>
-    <!-- Só faz o nossos placeholders de imagens funcionarem. Não precisar copiar a próxima linha! -->
-    <script src="../../assets/js/vendor/holder.min.js"></script>
-  </body>
+<hr class="featurette-divider">
+
+<div class="row featurette">
+<div class="col-md-7">
+<h2 class="featurette-heading">Primeiro título de featurezinhas. <span class="text-muted">Supreendente, não?!</span></h2>
+<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+</div>
+<div class="col-md-5">
+<img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+</div>
+</div>
+
+<hr class="featurette-divider">
+
+<div class="row featurette">
+<div class="col-md-7 order-md-2">
+<h2 class="featurette-heading">Aêêê, moleque! <span class="text-muted">Tá legal ou não tá?</span></h2>
+<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+</div>
+<div class="col-md-5 order-md-1">
+<img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+</div>
+</div>
+
+<hr class="featurette-divider">
+
+<div class="row featurette">
+<div class="col-md-7">
+<h2 class="featurette-heading">E, por último, essa aqui. <span class="text-muted">Xeque-mate!</span></h2>
+<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+</div>
+<div class="col-md-5">
+<img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+</div>
+</div>
+
+<hr class="featurette-divider">
+
+<!-- /FIM DAS FEATUREZINHAS *-* -->
+
+</div><!-- /.container -->
+
+
+<!-- FOOTER -->
+<footer class="container">
+<p class="float-right"><a href="#">Voltar ao topo</a></p>
+
+<p>&copy; PuroSol 2020 &middot; <a href="#">Privacidade</a> &middot; <a href="#">Termos</a></p>
+
+</footer>
+</main>
+
+<!-- Principal JavaScript do Bootstrap
+================================================== -->
+<!-- Foi colocado no final para a página carregar mais rápido -->
+<script src="{{ asset('js/jquery-3.3.1.slim.min.js')}}"></script>
+
+<script src="{{ asset('js/popper.min.js')}}"></script>
+
+<script src="{{ asset('js/bootstrap.min.js')}}"></script>
+
+<!-- Só faz o nossos placeholders de imagens funcionarem. Não precisar copiar a próxima linha! -->
+<script src="{{ asset('js//holder.min.js')}}"></script>
+
+</body>
 </html>
